0385a4b89e3048eeefffafe7a1966d4dceaefbdd
dropella-dev <austinandogola@gmail.com>
2025-05-28 21:17:01 +0300

    downloaders & remote caching al

--- downloaders.py
+++ downloaders.py
@@ -4,6 +4,8 @@ import requests
 import os
 from dotenv import load_dotenv
 import time
+import threading
+from b2_helper import upload_to_b2,file_exists_in_b2,download_from_b2
 
 load_dotenv()
 
@@ -142,10 +144,13 @@ def donwloader_one(vidId,max_retries=1):
 
 # donwloader_one('k5KxoG-Oi-A')
 
+method_one_host = os.getenv('METHOD_ONE_HOST')
+method_two_host = os.getenv('METHOD_TWO_HOST')
+method_three_host = os.getenv('METHOD_THREE_HOST')
 def download_method_one(vidId,sStart=None,sEnd=None,max_retries=3):
    
     start_time = time.time()
-    url = "https://youtube-mp36.p.rapidapi.com/dl"
+    url = f"https://{method_one_host}/dl"
     
     try:
         querystring = {"id":vidId}
@@ -160,7 +165,7 @@ def download_method_one(vidId,sStart=None,sEnd=None,max_retries=3):
 
         headers = {
             "x-rapidapi-key": RAPIDAPI_KEY,
-            "x-rapidapi-host": "youtube-mp36.p.rapidapi.com"
+            "x-rapidapi-host": method_one_host
         }
         
         response = requests.get(url, headers=headers, params=querystring)
@@ -205,17 +210,235 @@ def download_method_one(vidId,sStart=None,sEnd=None,max_retries=3):
         })
 
    
-
-
-      
-  
-
-def major_downloader(vidId,start=None,end=None):
-    answer = download_method_one(vidId,start,end)
     
-    print(answer)
+def download_method_three(vidId):
+    start_time = time.time()
+    try:
+        
+        url = f"https://{method_three_host}/dl"
+
+        print(url)
+        querystring = {"id":vidId}
+
+        headers = {
+            "x-rapidapi-key": RAPIDAPI_KEY,
+            "x-rapidapi-host": method_three_host
+        }
+
+        response = requests.get(url, headers=headers, params=querystring)
+        
+        response_json = response.json()
+        
+        # print(response_json)
+        
+        adaptiveFormats = response_json['adaptiveFormats']
+        
+        audio_formats = [item for item in adaptiveFormats if 'audio' in item['mimeType']]
+        
+        sorted_audio_formats = sorted(audio_formats, key=lambda item: item['bitrate'])
+        sorted_audio_bitrates = sorted(item['bitrate'] for item in sorted_audio_formats )
+        chosen_format = sorted_audio_formats[0]
+        
+        print(sorted_audio_bitrates)
+        print('chosen_format',chosen_format['bitrate'],chosen_format['averageBitrate'])
+        
+        download_link = chosen_format["url"]
+        
+        file_name =f"{vidId}.mp3"
+        
+        file_path = os.path.join(DOWNLOAD_DIR, file_name)
+        
+        print('Starting download 2')
+        
+        mp3_response = requests.get(download_link)
+        
+        print(mp3_response)
+    
+        
+        mp3_response.raise_for_status()  # Ensure the download was successful
+        
+        with open(file_path, 'wb') as file:
+           for chunk in mp3_response.iter_content(chunk_size=1048576):
+               if chunk:
+                   file.write(chunk)
+                    
+                    
+        download_time = time.time() - start_time
+
+        # Return the file path and download time
+        #return file_name, download_time
+        print('Downloadded in',download_time)
+        return ({
+            'file_path': file_path,
+            'download_time_seconds': download_time
+        })
+     
+     
+    except Exception as e:
+        return({
+            "error":True,
+            "message":repr(e)
+        })
+    
+    
+def download_method_two(vidId):
+    start_time = time.time()
+    try:
+        
+        url = f"https://{method_two_host}/download"
+
+        print(url)
+        querystring = {"url":f"https://www.youtube.com/watch?v={vidId}","format":"mp3"}
+
+        payload = {}
+        headers = {
+            "x-rapidapi-key": RAPIDAPI_KEY,
+            "x-rapidapi-host":method_two_host ,
+            "Content-Type": "application/json"
+        }
+
+        response = requests.post(url, headers=headers, params=querystring)
+     
+        
+        response_json = response.json()
+        
+        print(response_json)
+        
+        
+        item_id= response_json['id']
+        
+        print(item_id)
+        
+        status_url=  f"https://{method_two_host}/status/{item_id}"
+        
+        status= ''
+        
+        while(status != 'AVAILABLE'):
+            print(status)
+            response = requests.get(status_url, headers=headers)
+            response_json = response.json()
+            # print('response_json',response_json)
+            print(response_json['status'])
+            status = response_json['status']
+            
+            time.sleep(2.5)
+            
+        
+            
+        
+        download_link = response_json["downloadUrl"]
+        
+        file_name =f"{vidId}.mp3"
+        
+        file_path = os.path.join(DOWNLOAD_DIR, file_name)
+        
+        print('Starting download 2')
+        
+        mp3_response = requests.get(download_link)
+        
+        print(mp3_response)
+    
+        
+        mp3_response.raise_for_status()  # Ensure the download was successful
+        
+        with open(file_path, 'wb') as file:
+           for chunk in mp3_response.iter_content(chunk_size=1048576):
+               if chunk:
+                   file.write(chunk)
+                    
+                    
+        download_time = time.time() - start_time
+
+        # Return the file path and download time
+        #return file_name, download_time
+        print('Downloadded in',download_time)
+        return ({
+            'file_path': file_path,
+            'download_time_seconds': download_time
+        })
+     
+     
+    except Exception as e:
+        return({
+            "error":True,
+            "message":repr(e)
+        })
+
+DOWNLOAD_BUCKET_NAME = os.getenv("DOWNLOAD_BUCKET_NAME")
+def major_downloader(vidId,start=None,end=None):
+    
+    start_time = time.time()
+    
+    file_name =f"{vidId}.mp3"
+        
+    file_path = os.path.join(DOWNLOAD_DIR, file_name)
+    
+    if os.path.exists(file_path):
+        if not file_exists_in_b2(file_name,DOWNLOAD_BUCKET_NAME):
+            print("Uploading file")
+            thread = threading.Thread(target=upload_to_b2, args=( file_path,file_name, os.getenv("DOWNLOAD_BUCKET_NAME")))
+            thread.start()
+        return ({
+            'file_path': file_path,
+            'download_time_seconds': 'immediate'
+        })
+    else:
+        if(file_exists_in_b2(file_name,DOWNLOAD_BUCKET_NAME)):
+            print("File exists in bucket. Fetching")
+            ans = download_from_b2(file_name,DOWNLOAD_BUCKET_NAME,file_path,start_time)
+            
+            return ans
+    
+    trial = 0
+    
+    
+    
+    while(trial<1):
+        
+        answer = download_method_one(vidId,start,end)
+        print(answer)
+        if('file_path' in answer):
+           break 
+       
+        time.sleep(5) 
+       
+        trial=trial+1
+        
+        
+    if 'file_path' not in answer:
+        trial=0
+        while(trial<1):
+        
+            answer = download_method_two(vidId)
+            print(answer)
+            if('file_path' in answer):
+                break 
+            
+            time.sleep(5) 
+        
+            trial=trial+1
+    
+    if 'file_path' not in answer:
+        trial=0
+        while(trial<1):
+        
+            answer = download_method_three(vidId)
+            print(answer)
+            if('file_path' in answer):
+                break 
+            
+            time.sleep(5) 
+        
+            trial=trial+1
+    
+    if "file_path" in answer:
+        print('Uploading file')
+        thread = threading.Thread(target=upload_to_b2, args=( answer['file_path'],file_name, os.getenv("DOWNLOAD_BUCKET_NAME")))
+        thread.start()
+
     
     return answer
     
     
-# major_downloader('k5KxoG-Oi-A',"00:10:00")
+# ll = major_downloader('UxxajLWwzqY',"00:10:00")
+# print(ll)
